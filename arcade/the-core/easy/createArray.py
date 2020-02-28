a830bb81378da54cb15681e45b0a972b70cc729f
Logan Rowe <rowe1227@gmail.com>
2020-02-27 23:52:43 -0600

    adding array practice

---
+++ arcade/the-core/easy/createArray.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 23:47:49 2020
+
+@author: Logan Rowe
+
+Given an integer size, return array of length size filled with 1s.
+
+Example
+
+For size = 4, the output should be
+createArray(size) = [1, 1, 1, 1].
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] integer size
+
+A positive integer.
+
+Guaranteed constraints:
+1 ≤ size ≤ 1000.
+
+[output] array.integer
+
+[Python3] Syntax Tips
+"""
+
+def createArray(size):
+    return [1]*size
